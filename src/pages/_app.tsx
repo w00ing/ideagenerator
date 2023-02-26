5404d03f4ba8db837e353d1ff9f70103e560f6be
Woo-yeong Kim <kimwy1997@gmail.com>
2023-02-26 15:46:43 +0900

    chore: add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import { Toaster } from '@/components/ui/Toaster';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Toaster />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
